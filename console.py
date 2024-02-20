bce176e5631175ee65187df253605e00c139b7d5
Edwin K. Orioki <edwin.orioki@gmail.com>
2024-02-19 19:21:23 -0500

    added the sql script

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,16 +115,46 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
-            print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+        ignored_attrs = ('id', 'created_at', 'updated_at', '__class__')
+        try:
+            if not args:
+                raise SyntaxError ("** class name missing **")
+            
+            """Splitting the args into by spaces"""
+            arg_list = args.split()
+
+            """Getting the class name and attributes """
+            class_name = arg_list[0]
+            attributes = arg_list[1:]
+
+            """Checcking if the class exists"""
+            if class_name not in HBNBCommand.classes:
+                raise SyntaxError("** class doesn't exist **")
+            
+            """Parse attributes to create keyword arguments"""
+            kw_args = {}
+
+            for attr in attributes:
+                key, value = attr.split("=")
+                if key in ignored_attrs: 
+                    continue
+                value = eval(value)
+                if isinstance(value, str):
+                    value = value.replace("_", " ").replace("\"", "")
+                kw_args[key] = value
+            
+            """Creating an instance of the class witht he provided attributes"""
+            new_instance = HBNBCommand.classes[class_name](**kw_args)
+            new_instance.save()
+            print(new_instance.id)
+        except SyntaxError as e:
+            print(e)
+        except NameError as e:
+            print(e)
+        except ValueError as e:
+            print(e)
+        except Exception as e:
+            print(f"** Error Occurred: {e} **")
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +302,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +310,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
